822af4afb4a80964abeb85c27854048ef43c1a9f
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-21 17:29:43 +0200

    Add class Graphic

---
+++ src/main/java/org/example/simulation/phase/Graphic.java
@@ -0,0 +1,10 @@
+package org.example.simulation.phase;
+
+import org.example.simulation.configuration.Context;
+
+public class Graphic implements Phase{
+    @Override
+    public void perform(Context context) {
+
+    }
+}
