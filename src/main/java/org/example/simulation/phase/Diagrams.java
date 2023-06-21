acdc6419eea972c119b520dac68b19806b6d244e
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-21 17:29:03 +0200

    Add class Diagrams

---
+++ src/main/java/org/example/simulation/phase/Diagrams.java
@@ -0,0 +1,10 @@
+package org.example.simulation.phase;
+
+import org.example.simulation.configuration.Context;
+
+public class Diagrams implements Phase{
+    @Override
+    public void perform(Context context) {
+
+    }
+}
