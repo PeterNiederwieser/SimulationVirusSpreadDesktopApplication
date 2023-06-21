d256d25de417bd9711e622ad26c237e739aa1b07
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-21 17:28:00 +0200

    Add class Analysis

---
+++ src/main/java/org/example/simulation/phase/Analysis.java
@@ -0,0 +1,10 @@
+package org.example.simulation.phase;
+
+import org.example.simulation.configuration.Context;
+
+public class Analysis implements Phase{
+    @Override
+    public void perform(Context context) {
+
+    }
+}
