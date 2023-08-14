6f21e74de78343b1d3d17da8ec27462990de8674
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-08-14 13:50:52 +0200

    Remove no longer needed imports in Simulator

--- src/main/java/org/example/simulation/Simulator.java
+++ src/main/java/org/example/simulation/Simulator.java
@@ -1,28 +1,20 @@
 package org.example.simulation;
 
-import org.example.simulation.logic.initialisation.Initializer;
-import org.example.simulation.logic.io.MapCreator;
 import org.example.simulation.data.Context;
 import org.example.simulation.logic.simulationPhase.Phase;
 
-import java.io.IOException;
 import java.util.List;
 
 public class Simulator {
     private final Context context;
     private final List<Phase> phases;
-    private final MapCreator mapCreator;
-    private final Initializer initializer;
 
-    public Simulator(Context context, List<Phase> phases, MapCreator mapCreator, Initializer initializer) {
+    public Simulator(Context context, List<Phase> phases) {
         this.context = context;
         this.phases = phases;
-        this.mapCreator = mapCreator;
-        this.initializer = initializer;
     }
 
-    public void simulate() throws IOException {
-        // initializer.initializeSimulation();
+    public void simulate() {
         do {
             if (!context.isSimulationPaused()) {
                 simulatePhases();
@@ -41,5 +33,4 @@ public class Simulator {
             phase.perform(context);
         }
     }
-
 }
